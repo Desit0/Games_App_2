c0f183b9a73f9d2eb3318bc560b7c03c85f0bcac
Desit0 <desislavapd@students.npmg.org>
2023-01-06 17:32:58 +0200

    w

--- src/game_app_/Hangman_1.java
+++ src/game_app_/Hangman_1.java
@@ -13,8 +13,6 @@ public class Hangman_1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    
     
 }
