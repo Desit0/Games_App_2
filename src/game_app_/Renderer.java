2ab5035bcb0377c2950002d954d7f294c85d737e
Desit0 <desislavapd@students.npmg.org>
2023-01-10 21:53:03 +0200

    gotovo

---
+++ src/game_app_/Renderer.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package game_app_;
+
+/**
+ *
+ * @author Desi
+ */
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class Renderer extends JPanel
+{
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+
+		FlappyBird.flappyBird.repaint(g);
+	}
+	
+}
+
